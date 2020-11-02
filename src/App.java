132819e937398d412c4400aa106063fbb5486112
knutsun <csselph@gmail.com>
2020-11-02 16:18:28 -0700

    revise App

--- src/App.java
+++ src/App.java
@@ -4,18 +4,19 @@ public class App {
 
         Queue<Object> queue = new Queue<>();
 
-        // enqueue integers 1..3
+        // enqueue Objects: in this case, the integers 0, 1, 2
         for(int i = 0; i < 3; i++)
-            queue.enqueue(i);
+            queue.enqueue("Iteration number: " + i);
 
         // execute 2 dequeue operations
-        System.out.println(queue.size());
+        System.out.println("Size of queue: " + queue.size());
+
         for(int i = 0; i < queue.size(); i++)
-            System.out.println("Dequeing: " + queue.dequeue());
+            System.out.println("Dequeuing: " + queue.dequeue());
 
         // dequeue the rest
         while(!queue.isEmpty())
-            System.out.println("Finally Dequeing: " + queue.dequeue());
+            System.out.println("Finally Dequeuing: " + queue.dequeue());
     }
 
 }
